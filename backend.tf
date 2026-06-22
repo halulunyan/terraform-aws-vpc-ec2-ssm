266a7b4425464b365fcb48d02d33a9349c8dd5ae
halu <halulunyan@users.noreply.github.com>
2026-06-22 17:41:24 +0900

    Remove backend configuration with real bucket name

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "terraform-state-yoshihiro-941960167304-ap-northeast-1"
-    key          = "terraform-aws-vpc-ec2-ssm/terraform.tfstate"
-    region       = "ap-northeast-1"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
